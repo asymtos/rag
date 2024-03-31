2904cd96fde8009f21e8460b2d2a3c31c095c81e
nishaa077 <111753617+nishaa077@users.noreply.github.com>
2024-03-31 15:15:25 +0530

    Delete call.py

--- call.py
+++
@@ -1,8 +0,0 @@
-from store import store_to_pinecone, process_query_text
-
-str="installation error: Module not found"
-list=["installation error: Module not found"]
-
-#store_to_pinecone(list)
-
-process_query_text(str)
