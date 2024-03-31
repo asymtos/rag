386598e7ff38b999848184fe35c37fc2615cf9df
nishaa077 <111753617+nishaa077@users.noreply.github.com>
2024-03-31 15:23:18 +0530

    Update call.py

--- backend/call.py
+++ backend/call.py
@@ -1,8 +1,11 @@
 from store import store_to_pinecone, process_query_text
 
+#examples
 str="installation error: Module not found"
 list=["installation error: Module not found"]
 
-#store_to_pinecone(list)
+#to store the embedding in the pinecone
+store_to_pinecone(list)
 
+#To get the top=k results of most similar vector
 process_query_text(str)
